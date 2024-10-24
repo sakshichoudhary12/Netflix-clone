a51ee081789701616c9d7d87eaffb1aefbacf6f6
sakshichoudhary12 <137709066+sakshichoudhary12@users.noreply.github.com>
2024-10-24 21:10:35 +0530

    Add files via upload

---
+++ frontend/src/utils/constant.js
@@ -0,0 +1,18 @@
+export const API_END_POINT = "http://localhost:8010/api/v1/user";
+
+export const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzA2MTE4NWZiMzBmOGViZWRjOTE3MGEwMjFmM2U2NSIsIm5iZiI6MTcyOTYwNzg3NC4xOTgyMzEsInN1YiI6IjY3MTdiNjdlMTAxOTBkMjRhZjlhZmEwMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0LIduWPHxhbFu-k_fJklSpnKAmLMh4x6FJtr6qi1hHY'
+    }
+};
+
+export const Now_Playing_Movie = "https://api.themoviedb.org/3/movie/now_playing";
+export const Popular_Movies = "https://api.themoviedb.org/3/movie/popular";
+export const Top_Rated_Movies = "https://api.themoviedb.org/3/movie/top_rated";
+export const Upcoming_Movies = "https://api.themoviedb.org/3/movie/upcoming";
+
+export const SEARCH_MOVIE_URL = "https://api.themoviedb.org/3/search/movie?query="
+
+export const TMDB_IMG_URL = "https://image.tmdb.org/t/p/w500";
